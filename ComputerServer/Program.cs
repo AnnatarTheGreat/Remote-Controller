a893bfcfab508a33f1db146dd0e4f27eb87881cf
Roman Mihalev <roman.mihalev002@gmail.com>
2024-06-06 00:35:01 +0300

    initial commit

---
+++ ComputerServer/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+
+[DllImport("kernel32.dll")]
+static extern IntPtr GetConsoleWindow();
+[DllImport("user32.dll")]
+static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+const int SW_HIDE = 0;
+var handle = GetConsoleWindow();
+ShowWindow(handle, SW_HIDE);
+
+
+var ipAdress = IPAddress.Any;
+IPEndPoint iPEndPoint = new IPEndPoint(ipAdress, 8888);
+TcpListener server = new TcpListener(iPEndPoint);
+string token = "some token";
+string batFilePath = "Death.bat";
+try
+{
+    server.Start();
+    while (true)
+    {
+        TcpClient client = await server.AcceptTcpClientAsync();
+        
+        _ = Task.Run(async () => ListenForMessage(client));
+    }
+}
+finally
+{
+    server.Stop();
+}
+
+
+async void ListenForMessage(TcpClient client)
+{
+    NetworkStream stream = client.GetStream();
+    byte[] buffer = new byte[client.ReceiveBufferSize];
+    int bytesRead = 0;
+    while (client.Connected)
+    {
+        try
+        {
+            bytesRead = stream.Read(buffer, 0, client.ReceiveBufferSize);
+        }
+        catch
+        {
+            break;
+        }
+        if (bytesRead > 0)
+        {
+            string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            if (message == token)
+            {
+                Process process = new Process();
+                process.StartInfo.FileName = batFilePath;
+                process.StartInfo.UseShellExecute = true; 
+                process.StartInfo.CreateNoWindow = true;
+
+                process.Start();
+            }
+        }
+        else
+        {
+            break;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ RemoteController/Killer/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace Killer
+{
+    public partial class MainPage : ContentPage
+    {
+        private TcpClient _client;
+        private NetworkStream _stream;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnConnectClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await Connect();
+                ConnectBtn.Text = "Connected!";
+                SemanticScreenReader.Announce(ConnectBtn.Text);
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", $"Connection failed: {ex.Message}", "OK");
+            }
+        }
+
+
+        private void OnTurnOffClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                string token = "it can be litterally any token";
+                byte[] bytesToSend = Encoding.UTF8.GetBytes(token);
+                _stream.Write(bytesToSend, 0, bytesToSend.Length);
+                CounterBtn.Text = "WELL DONE!";
+                SemanticScreenReader.Announce(CounterBtn.Text);
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Error", $"Failed to send message: {ex.Message}", "OK");
+            }
+        }
+
+
+        public async Task Connect()
+        {
+            _client = new TcpClient();
+            await _client.ConnectAsync("322.228.6.66", 8888);
+            _stream = _client.GetStream();
+        }
+    }
+}
